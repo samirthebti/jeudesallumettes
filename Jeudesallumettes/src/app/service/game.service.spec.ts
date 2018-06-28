238b69e8652b113886c8a0502979818207655b1b
Samir Thebti <thebtisam@gmail.com>
2018-06-28 11:03:30 +0200

    test: add test cases gor gameservice

--- Jeudesallumettes/src/app/service/game.service.spec.ts
+++ Jeudesallumettes/src/app/service/game.service.spec.ts
@@ -2,13 +2,13 @@
 import {TestBed, getTestBed} from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {GameService} from "./game.service";
+import {RoundModel} from "../entities/round.model";
 
 describe('Service Tests', () => {
 
   describe('Client Service', () => {
     let injector: TestBed;
     let service: GameService;
-    let httpMock: HttpTestingController;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
@@ -16,17 +16,25 @@ describe('Service Tests', () => {
           HttpClientTestingModule
         ],
         providers: [
-
           GameService
         ]
       });
       injector = getTestBed();
       service = injector.get(GameService);
-      httpMock = injector.get(HttpTestingController);
+    });
+
+    describe('Service methods', () => {
+      it('should create a round', () => {
+        let game = service.create(21);
+        expect(game.winner).toEqual(null);
+        expect(game.totalitems).toEqual(21);
+      });
+      it('should fail when remove items from empty round', () => {
+        expect(service.removeAllumette(3, null, "")).toBeTruthy();
+      });
     });
 
     afterEach(() => {
-      httpMock.verify();
     });
 
   });
